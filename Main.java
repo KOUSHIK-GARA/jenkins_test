3b816b8344f135baf098d2de5c56389377036ee0
KOUSHIK <koushikgara.dev@gmail.com>
2024-01-13 09:21:16 +0530

    "failed student"

--- Main.java
+++ Main.java
@@ -7,7 +7,6 @@ public class Main{
         System.out.println("Hi I'm koushik");
         System.out.println("Let\'s Jenkin");
         System.out.println("Added changes");
-        Student s = new Student();
-        s.sayHello();
+        System.out.println("failed Student");
     }
 }
