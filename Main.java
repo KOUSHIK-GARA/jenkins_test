45be1983bb86911fa02faa9f3b358cd43e2ac954
KOUSHIK <koushikgara.dev@gmail.com>
2024-01-13 09:05:31 +0530

    "Added JAVA FILE"

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main{
+
+    public static void main(String args[]){
+        System.out.println(new Date());
+        System.out.println("Hi I'm koushik");
+        System.out.println("Let\'s Jenkin");
+    }
+}
